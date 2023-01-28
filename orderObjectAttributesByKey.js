83a23c844af80a7c44f990276832459940ab3868
pierocarrion <piero.january15@gmail.com>
2023-01-27 22:05:35 -0500

    Order Object Attributes By Key Added

---
+++ orderObjectAttributesByKey.js
@@ -0,0 +1,10 @@
+
+
+function orderObjectAttributesByKey(unordered) {
+    return Object.keys(unordered)
+        .sort()
+        .reduce((obj, key) => {
+            obj[key] = unordered[key]
+            return obj;
+        }, {});
+}
